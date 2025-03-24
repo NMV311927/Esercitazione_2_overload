b8d30863e6430f3fc88c3da571ee44a72c9b7c6c
Matteo  Villa <matteovilla@Matteos-Air-2.homenet.telecomitalia.it>
2025-03-24 14:37:14 +0100

    should be done now

--- overload.cpp
+++ overload.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include "overload.hpp"
 
-
-
 int main(void) {
 
-    using clx = complex_number<int>;
+    using clx = complex_number<double>;
+    clx c1(1.5, 2.3);
+    clx c2(3.3, 1.0);
+
+    std::cout << c1 << " + " << c2 << " = " << c1+c2 << std::endl;
+    clx c3 = c1+c2;
+    std::cout << "The conjugate of " << c3 << " is " << conj(c3) << "; ";
+    std::cout << "its real part is " << Re(c3)<< ", and its imaginary part is " << Im(c3) << std::endl; 
     
+    //
+
+    clx c5 = clx(3.4,4.2) + 2.2;
+    std::cout << "(3.4 + 4.2i) + 2.2 = " << c5 << std::endl;
+    clx c6 = 3.1 + clx(5.3,8.7);
+    std::cout << "3.1 + (5.3 + 8.7i) = " << c6 << std::endl;
+    
+    return 0;
 }
--- overload.hpp
+++ overload.hpp
@@ -6,7 +6,7 @@ class complex_number{
     I a, b;
 
 public:
-    complex_number(): a(1), b(1) {}
+    complex_number(): a(0), b(0) {}
 
     explicit complex_number(I r): a(r), b(0) {
         std::cout << "converting constructor called (real->complex)" << std::endl;
@@ -28,7 +28,7 @@ public:
         return b;
     }
 
-
+    //
 
     complex_number& operator+=(const complex_number& other) {
         I c = a;
@@ -45,9 +45,49 @@ public:
         ret += other;
         return ret;
     }
+
+    complex_number& operator+=(const I& other) {
+        complex_number add(other);
+        a += add;
+        return *this;
+	}
+
+	complex_number operator+(const I& other) const {
+        complex_number ret = *this;
+        ret += other;
+        return ret;
+    }
+
+    //
+
+    ccomplex_number& operator*=(const complex_number& other){
+        I c = a;
+        I d = b;
+        I e = other.a;
+        I f = other.b;
+        a = c*e - d*f;
+        b = d*e + c*f;
+        return *this;
+    }
+
+    complex_number operator*(const complex_number& other) const {
+        complex_number ret = *this;
+        ret *= other;
+        return ret;
+    }
+
+    complex_number& operator*=(const I& other) {
+        complex_number mol(other);
+        a *= mol;
+        return *this;
+	}
+
+	complex_number operator*(const I& other) const {
+        complex_number ret = *this;
+        ret *= other;
+        return ret;
+    }
 };
-
-
 
 template<typename I>
 complex_number<I> conj(const complex_number<I>& r){
@@ -64,10 +104,26 @@ complex_number<I> Im(const complex_number<I>& r){
     return complex_number<I>(0, r.im());
 }
 
+//
+
+template<typename I>
+complex_number<I> operator+(const I& i, const complex_number<I>& r)
+{
+    return r + i;
+}
+
+template<typename I>
+complex_number<I> operator*(const I& i, const complex_number<I>& r)
+{
+    return r * i;
+}
+
+//
+
 template<typename I>
 std::ostream& operator<<(std::ostream& os, const complex_number<I>& r) {
     if (r.im() < 0)
-        os << r.rl() << " - " << r.im() << "i";
+        os << r.rl() << " - " << -r.im() << "i";
     else 
         os << r.rl() << " + " << r.im() << "i";
 
